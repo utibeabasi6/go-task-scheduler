88be2ff35f1071c26e6f7bfb42d1a67b5f2cecaf
Utibeabasi Umanah <utibeabasiumanah6@gmail.com>
2023-10-16 08:40:42 +0100

    fix: remove unused struct

--- worker/structs.go
+++ worker/structs.go
@@ -1,13 +1,5 @@
 package main
 
-type RedisConfig struct {
-	Url string `json:"url"`
-}
-type Config struct {
-	Port  int         `json:"port"`
-	Redis RedisConfig `json:"redisConfig"`
-}
-
 type Job struct {
 	Id      string `json:"jobId"`
 	Type    string `json:"jobType"`
